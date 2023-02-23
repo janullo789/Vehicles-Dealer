301ee3abac8081198fb676f9eaadd0da3c1d0109
Jan Skwarczenski <skwarko@student.agh.edu.pl>
2023-02-23 23:07:34 +0100

    Bicycle class

--- Bicycle.cpp
+++ Bicycle.cpp
@@ -4,6 +4,8 @@
 
 #include "Bicycle.h"
 
+#include <iostream>
+
 using namespace std;
 
 Bicycle::Bicycle(char type, string model, string color, float price, float capacity, int mileage, bool rack)
@@ -15,3 +17,56 @@ Bicycle::Bicycle(char type, string model, string color, float price, float capac
         this->capacity = 0;
 }
 
+void Bicycle::drive()
+{
+    string km;
+    cout << "How many kilometers do you want to ride? " << endl;
+    cin >> km;
+
+    try
+    {
+        int change = controlTypeINT(km);
+        mileage += controlDriveB(change);
+        cout << "You've reached your destination" << endl;
+    }
+    catch(ErrorOfType &m)
+    {
+        cerr << "Invalid value" << endl;
+        cerr << m.message << endl;
+    }
+    catch(ErrorOfValue &m)
+    {
+        cerr << "Invalid value" << endl;
+        cerr << m.message << endl;
+    }
+
+    cout << "End of the bicycle ride" << endl;
+}
+
+void Bicycle::refuel()
+{
+    cout << "I am a bicycle! You fuel me with your muscles, so you don't have to worry about refueling :)" << endl;
+}
+
+void Bicycle::printOn(ostream &o) const
+{
+    o << "Model: " << model << " Color: " << color << " Price: " << price << " Capacity: " << capacity << " Mileage: " <<
+      mileage << " Rack: " << rack << endl;
+}
+
+void Bicycle::saveOn(ofstream &o) const
+{
+    o << type << " " << model << " " << color << " " << price << " " << capacity << " " << mileage << " " << rack << endl;
+}
+
+int Bicycle::controlDriveB(int &k)
+{
+    if(k >= 0)
+        return k;
+    else
+    {
+        ErrorOfType sender;
+        sender.message = "You cannot drive a negative number of km";
+        throw sender;
+    }
+}
