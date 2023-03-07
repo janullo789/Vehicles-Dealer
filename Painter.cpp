5b57e3df837d3d2bea9418cec82d4bdeea6a3c33
Jan Skwarczenski <skwarko@student.agh.edu.pl>
2023-03-07 10:30:41 +0100

    Painter class finish

--- Painter.cpp
+++ Painter.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "Painter.h"
+
+using namespace std;
+
+Painter::Painter() = default;
+
+Painter::Painter(unique_ptr<Vehicle>& paintingVehicle) : paintingVehicle(std::move(paintingVehicle)) {}
+
+void Painter::painting()
+{
+    char choice;
+    string color;
+
+    do
+    {
+        cout << "Select new vehicle color" << endl;
+        cout << "1. White" << endl <<
+             "2. Black" << endl <<
+             "3. Silver" << endl <<
+             "4. Red" << endl <<
+             "5. Gold" << endl <<
+             "6. Green" << endl;
+        cin >> choice;
+
+        switch (choice)
+        {
+            case '1':
+                color = "White";
+                break;
+            case '2':
+                color = "Black";
+                break;
+            case '3':
+                color = "Silver";
+                break;
+            case '4':
+                color = "Red";
+                break;
+            case '5':
+                color = "Gold";
+                break;
+            case '6':
+                color = "Green";
+            default:
+                cout << "Enter number between 1 - 6" << endl;
+                break;
+
+        }
+    } while(choice != '1' && choice != '2' && choice != '3' && choice != '4' && choice != '5' && choice != '6');
+
+    paintingVehicle->repaint(color);
+    fromPainting = move(paintingVehicle);
+}
--- Painter.h
+++ Painter.h
@@ -5,13 +5,20 @@
 #ifndef CAR_DEALER_PAINTER_H
 #define CAR_DEALER_PAINTER_H
 
+#include <iostream>
 #include <memory>
 #include "Vehicle.h"
 
 class Painter
 {
 public:
-    
+    Painter();
+    Painter(std::unique_ptr<Vehicle>&);
+    void painting();
+    std::unique_ptr<Vehicle> fromPainting;
+
+private:
+    std::unique_ptr<Vehicle> paintingVehicle;
 };
 
 
